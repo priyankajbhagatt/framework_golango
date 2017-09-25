5a06be09a9495ddef10cfcdb01e6d13376db2e05
System Administrator <root@Jayeshs-MacBook-Pro.local>
2017-09-26 00:12:22 +0530

    Welcome page added @ready

---
+++ Creator/Template/index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE HTML>
+<!--
+	Photon by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Framework Golango : Team Golango</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="Creator/Template/assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Header -->
+			<section id="header">
+				<div class="inner">
+					<span class="icon major fa-cloud"></span>
+					<h1>Hi, I'm <strong>Framework Golango</strong>, another fine<br />
+					little and lightweight frameowrk created by <a href="http://html5up.net">Team Golango</a>.</h1>
+					<p>Project golango is freeware and currently under development project</p>
+					<ul class="actions">
+						<li><a href="#one" class="button scrolly">Discover</a></li>
+					</ul>
+				</div>
+			</section>
+
+		<!-- One -->
+			<section id="one" class="main style1">
+				<div class="container">
+					<div class="row 150%">
+						<div class="6u 12u$(medium)">
+							<header class="major">
+								<h2>Framework Golnago<br />
+								</h2>
+							</header>
+							<p>Framework Golango is lightweight framework to develop dynamic websites. Golango is currently underdevelopment project and written in PHP 7,Angular And even we are going to provide future support for Python.
+								<br />Our aim is to write and develop website using single framework for both Serverside and Client side development</p>
+
+								<p>Download Project From Github<br />
+
+									<span style="
+    background: lightblue;
+    color: black;
+    font-weight: bold;
+    padding: 5px;
+    border-radius: 5px;
+">https://github.com/priyankajbhagatt/framework_golango.git</span></p>
+						</div>
+						<div class="6u$ 12u$(medium) important(medium)">
+							<span class="image fit"><img src="images/pic01.jpg" alt="" /></span>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		<!-- Two -->
+		
+
+		<!-- Three -->
+		
+
+		<!-- Four -->
+			<section id="four" class="main style2 special">
+				<div class="container">
+					<header class="major">
+						<h2>Contribute your knwoledge to make Golango much better</h2>
+					</header>
+					<p></p>
+					<ul class="actions uniform">
+						<li><a href="#" class="button special">Stay Tuned!</a></li>
+						<li><a href="#" class="button">Learn More</a></li>
+					</ul>
+				</div>
+			</section>
+
+		
+
+		<!-- Footer -->
+			<section id="footer">
+				<ul class="icons">
+					<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+					<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+					<li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+					<li><a href="#" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
+					<li><a href="#" class="icon alt fa-envelope"><span class="label">Email</span></a></li>
+				</ul>
+				<ul class="copyright">
+					<li>Golangoframework.com</li><li>Design: <a href="http://html5up.net">Team Golango</a></li>
+				</ul>
+			</section>
+
+		<!-- Scripts -->
+			<script src="Creator/Template/assets/js/jquery.min.js"></script>
+			<script src="Creator/Template/assets/js/jquery.scrolly.min.js"></script>
+			<script src="Creator/Template/assets/js/skel.min.js"></script>
+			<script src="Creator/Template/assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="Creator/Template/assets/js/main.js"></script>
+
+	</body>
+</html>

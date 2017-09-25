1024166f0400c8e2f725e77cc175861f5ba5d701
System Administrator <root@Jayeshs-MacBook-Pro.local>
2017-09-26 00:18:03 +0530

    Changes for action

--- Creator/Action/First_Action.php
+++ Creator/Action/First_Action.php
@@ -10,17 +10,7 @@ class First_Action extends Action{
 	}
 	public function init(){
 
-		$this->Template('Template',array('id'=>1));
+		$this->Template('index',array('id'=>1));
 	}	
-	public function jayesh(){
-
-		$Object = $this->grab->prepareDatamodel('Checkout');
-		$return['checkout'] = $Object->Jay();
-		$return['newdata'] = '1234556';
-
-		$this->grab->Template('Template',$return);
-		
-	}
-
 
 }

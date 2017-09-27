e15a6323ca8af3f6c369be18352c6b2e8bca4952
Jayesh Prakash Bhagat <jayeshpbhagat@Jayeshs-MacBook-Pro.local>
2017-09-27 22:18:38 +0530

    CHanges commited for grab class created

--- Core/Action.php
+++ Core/Action.php
@@ -4,67 +4,15 @@ abstract class Action{
 
 	public $grab;
 	private $Template_Object;
-	public function __construct($params=array()){
-
-		$this->grab = $this;
-	}
-
-	protected function Template($Template,$Data=array()){
-
-		return $this->createTemplate($Template,$Data);
-
-	}
-
-	private function createTemplate($Template,$Data=array()){
 	
-		if(!empty($Template) && NULL != $Template){
-
-			$this->Template_Object = new Template();
-			$this->Template_Object->openTemplate($Template,$Data);
-
-		}
-
-
+	public function __construct($params=array()){
+		
+		return $this->grab = new Grab();
 	}
 
-	public function prepareDatamodel($Datamodel){
+	protected function execute_command($command,$params=array()){
 
-			if(!empty($Datamodel) && NULL != $Datamodel){
-
-					if(file_exists(__DIR_PATH__.'/Creator/Datamodel/'.$Datamodel.'.php')){
-
-						$this->_requie_file_model(__DIR_PATH__.'/Creator/Datamodel/'.$Datamodel.'.php');
-
-
-						return $Datamodel_Object = new $Datamodel();
-						
-
-					}else if(file_exists(__DIR_PATH__.'/Core/Datamodel/'.$Datamodel.'.php')){
-
-						$this->_requie_file_model(__DIR_PATH__.'/Core/Datamodel/'.$Datamodel.'.php');
-
-					}else{
-
-						die('Datamodel Not Found');
-					}
-
-			}	
-
-
-	}
-
-
-	private function _requie_file_model($Datamodelnamepath){
-
-		try {
-		    
-		    require_once($Datamodelnamepath);
-
-		} catch (ErrorException $ex) {
-
-		    die('Datamodel Not Found');
-		    
-		}
+		 return exec($command);
 
 	}
 
---
+++ Core/Grab.php
@@ -0,0 +1,82 @@
+<?php
+
+class Grab{
+
+	public $grab;
+	private $Template_Object;
+	public function __construct($params=array()){
+
+		return $this->grab = $this;
+	}
+
+	
+	public function Template($Template,$Data=array()){
+		
+		return $this->createTemplate($Template,$Data);
+
+	}
+
+	private function createTemplate($Template,$Data=array()){
+	
+		if(!empty($Template) && NULL != $Template){
+
+			$this->Template_Object = new Template();
+			$this->Template_Object->openTemplate($Template,$Data);
+
+		}
+
+
+	}
+
+	public function prepareDatamodel($Datamodel){
+
+			if(!empty($Datamodel) && NULL != $Datamodel){
+
+					if(file_exists(__DIR_PATH__.'/Creator/Datamodel/'.$Datamodel.'.php')){
+
+						$this->_requie_file_model(__DIR_PATH__.'/Creator/Datamodel/'.$Datamodel.'.php');
+
+
+						return $Datamodel_Object = new $Datamodel();
+						
+
+					}else if(file_exists(__DIR_PATH__.'/Core/Datamodel/'.$Datamodel.'.php')){
+
+						$this->_requie_file_model(__DIR_PATH__.'/Core/Datamodel/'.$Datamodel.'.php');
+
+					}else{
+
+						die('Datamodel Not Found');
+					}
+
+			}	
+
+
+	}
+
+
+	private function _requie_file_model($Datamodelnamepath){
+
+		try {
+		    
+		    require_once($Datamodelnamepath);
+
+		} catch (ErrorException $ex) {
+
+		    die('Datamodel Not Found');
+		    
+		}
+
+	}
+
+	/*private function loadDatamodel(''){
+
+
+
+	}*/
+
+
+	
+
+
+}
